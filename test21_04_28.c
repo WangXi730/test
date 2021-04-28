d7f741baec3a514606ce5007b07e90a124a5f6e6
WangXi <wangxi730403730@163.com>
2021-04-29 00:07:21 +0800

    来自21年4月29日的随手敲的代码

---
+++ test21_04_28.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<math.h>
+
+//int main() {
+//	int a = 1,b=10,c=0;
+//	for (; a < b; a++) {
+//		c = a | b;
+//		printf("%d\n", c);
+//	}
+//	return 0;
+//}
+//int account(int c) {
+//	if (c / 2 == 0) {
+//		return c;
+//	}
+//	return c % 2 + account(c/2);
+//}
+//int main() {
+//	//int a = 0;
+//	//int b = 0;
+//	//scanf("%d %d",&a, &b);
+//	//int c=a ^ b;
+//	int d = 0;
+//	scanf("%d", &d);
+//	d = account(d);
+//	printf("%d个1",d);
+//	return 0;
+//}
+
+//void print1(int a) {
+//	
+//}
+//int main() {
+//	int a = 0;
+//	print1(a);//奇数位
+//	print2(a);//偶数位
+//	return 0;
+//}
+
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("%d %d", a, b);
+//	return 0;
+//}
